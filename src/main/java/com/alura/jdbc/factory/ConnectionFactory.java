e4269286ec31cda0dad9677febad0c2bc892e986
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-05-19 17:19:25 -0500

    pool de conexiones con c3p0

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,25 @@
 package com.alura.jdbc.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    private DataSource dataSource;
+    public ConnectionFactory() {
+            var pooledDataSource = new ComboPooledDataSource();
+            pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+            pooledDataSource.setUser("root");
+            pooledDataSource.setPassword("WILFREDO1998");
+            pooledDataSource.setMaxPoolSize(10);
+
+            this.dataSource = pooledDataSource;
+    }
+
     public Connection recuperarConexion() throws SQLException {
-        Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                "WILFREDO1998");
-        return con;
+        return this.dataSource.getConnection();
     }
 }
